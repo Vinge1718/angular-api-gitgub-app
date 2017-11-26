c2c3f339fd48fd1510ad4cccbb0d88c904391104
Victor King'e <vinge1718@gmail.com>
2017-11-26 07:44:14 +0300

    add the http service injection necessary for running the function

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { catchError, map, tap } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService {
 
-  constructor() { }
+  
+
+  getUsers(param){
+    return this.http.get(param)
+  }
+
+  constructor(private http: HttpClient) { }
 
 }
