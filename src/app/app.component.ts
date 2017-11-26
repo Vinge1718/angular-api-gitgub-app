bd9c6c34de5d69b14146882a96f09326a42ea485
Victor King'e <vinge1718@gmail.com>
2017-11-26 07:40:07 +0300

    injecct the service necessary for fetching data and passing in the search parameter for the service to use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { userData } from './user';
 import { apiKey } from './env';
+import {Headers, RequestOptions} from '@angular/http';
+import { GithubService } from './github.service';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +17,28 @@ export class AppComponent {
 
   public userData: string;
 
-  inputKey: string = apiKey.inputKey;
+  public gitHubInfor: string;
+
+  public returnedUserData: any = null;
+
+  apiKey: string = apiKey.inputKey;
 
   searchInput(userToSearch: string){
     this.userData = userToSearch;
-    console.log(this.userData + this.inputKey)
+    this.gitHubInfor = "https://api.github.com/users/" + this.userData + "?access_token=" + this.apiKey;
+    this.getUsers();
   }
 
-  constructor() { }
+  constructor(private gitHubService: GithubService) { }
+
+
+  getUsers() {
+    this.gitHubService.getUsers(this.gitHubInfor)
+    .subscribe(resUserData => {
+      this.returnedUserData = resUserData;
+      console.log(this.returnedUserData);
+    });
+  }
 
   ngOnInit() {
   }
