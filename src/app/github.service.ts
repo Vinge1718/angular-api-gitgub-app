2bda13f5073fa50b9827586d5a2da8676133da7f
Victor King'e <vinge1718@gmail.com>
2017-11-26 08:41:00 +0300

    remove the unnecessry module imports

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Http, Response } from '@angular/http';
+
 import { of } from 'rxjs/observable/of';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { catchError, map, tap } from 'rxjs/operators';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class GithubService {
 
-  
+//The param variable used is defined in the click function where this function is called-back
+//After angular 4 the (where you import "HttpClient" instead of "http" modules) map() function isn't necessary and it will bring up an error
 
   getUsers(param){
     return this.http.get(param)
